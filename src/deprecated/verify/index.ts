0b7600ad7ee104359b02a614a73dc458bf16b677
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2026-06-12 13:46:43 +0100

    chore: switch build from tsup to tsdown, return to typescript 6, enable isolatedModules

--- src/deprecated/verify/index.ts
+++ src/deprecated/verify/index.ts
@@ -1,9 +1,11 @@
-export { VerifyReceiptRequestBody } from './requestbody'
-export {
-  VerifyReceiptErrorStatus,
+export type { VerifyReceiptRequestBody } from './requestbody'
+export type {
   VerifyReceiptResponseBody,
   VerifyReceiptResponseError,
   VerifyReceiptResponseSuccess,
+} from './responsebody'
+export {
+  VerifyReceiptErrorStatus,
   VerifyReceiptSuccessStatus,
 } from './responsebody'
 export { verifyReceipt, VerifyReceiptFetchError } from './verifyReceipt'
